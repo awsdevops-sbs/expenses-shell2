8f7a11f0e3840a6b79a370d6a1c377f2db3b79ed
akram <akram.khan200@gmail.com>
2025-07-24 10:03:28 +0530

    adding logs 13

--- frontend.sh
+++ frontend.sh
@@ -2,32 +2,29 @@ source common.sh
 
 Print_Task_Heading "Install Nginx"
 dnf install nginx -y &>>$Log
-echo $?
-
-
-
+Check_Status $?
 
 Print_Task_Heading "Copy Expense Conf"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$Log
-echo $?
+Check_Status $?
 
 Print_Task_Heading "Clean Old content"
 rm -rf /usr/share/nginx/html/* &>>$Log
-echo $?
+Check_Status $?
 
 Print_Task_Heading "Download Frontend Component"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>$Log
-echo $?
+Check_Status $?
 
 Print_Task_Heading "Extract and unzip frontend component "
 cd /usr/share/nginx/html &>>$Log
 unzip /tmp/frontend.zip &>>$Log
-echo $?
+Check_Status $?
 
 Print_Task_Heading "Start Nginx Service"
 systemctl enable nginx &>>$Log
 systemctl start nginx &>>$Log
 systemctl restart nginx &>>$Log
-echo $?
+Check_Status $?
 
 
--- mysql.sh
+++ mysql.sh
@@ -1,13 +1,26 @@
 source common.sh
-#My_root_password=$1
+My_root_password=$1
+
+if [ -z "${My_root_password}" ]; then
+
+  echo -e "\e[31mInput Password Missing\e[0m".
+  exit 1
+fi
 
 Print_Task_Heading "Install MySQL server"
-dnf install mysql-server -y &>>/tmp/expense.log
-echo $?
+dnf install mysql-server -y &>>$Log
+Check_Status $?
 
 Print_Task_Heading "Enable and Start MySQL server"
-systemctl enable mysqld &>>/tmp/expense.log
-systemctl start mysqld &>>/tmp/expense.log
-echo $?
+systemctl enable mysqld &>>$Log
+systemctl start mysqld &>>$Log
+Check_Status $?
 
-sudo mysql_secure_installation --set-root-pass ExpenseApp@1 &>>/tmp/expense.log
+Print_Task_Heading "Setup MySql password"
+echo 'show databases' | mysql -h 172.31.17.214 -uroot -p${My_root_password} &>>$Log
+if [ $? -ne 0 ]; then
+sudo mysql_secure_installation --set-root-pass ${My_root_password} &>>$Log
+
+fi
+
+Check_Status $?
